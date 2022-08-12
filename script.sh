bc7a1c6600e221edecb7800495cbe20929681254
jzinferno <jekazinferno@gmail.com>
2022-08-12 08:39:43 +0300

    Create setup script

---
+++ script.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+DIR=$(cd "$(dirname "$0")" && pwd)
+
+cd $DIR
+
+wget https://busybox.net/downloads/busybox-1.35.0.tar.bz2
+
+tar -xf busybox-1.35.0.tar.bz2
+
+mv $DIR/busybox-1.35.0 $DIR/busybox
+
+cd $DIR/busybox
+
+for i in ../patches/*.patch; do
+  patch -p1 < $i
+done
+
+cp ../busybox.config .config
+
+make oldconfig
+
+gcc applets/applet_tables.c -o applets/applet_tables
+applets/applet_tables include/applet_tables.h include/NUM_APPLETS.h
+gcc applets/usage.c -o applets/usage -Iinclude
+applets/usage_compressed include/usage_compressed.h applets
+scripts/mkconfigs include/bbconfigopts.h include/bbconfigopts_bz2.h
+scripts/generate_BUFSIZ.sh include/common_bufsiz.h
+srctree=$(pwd -P) HOSTCC=gcc scripts/embedded_scripts include/embedded_scripts.h embed applets_sh
